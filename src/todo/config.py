c7e1056e5538d905e5e50ca7152c0bd395ba4a61
Abdulroqeeb Bakre <walebakre10@gmail.com>
2025-09-03 23:59:48 +0100

    database: database config

---
+++ src/todo/config.py
@@ -0,0 +1,22 @@
+from starlette.config import Config
+from starlette.datastructures import Secret
+from urllib import parse
+config = Config(".env")
+
+
+
+#database
+DATABASE_HOSTNAME = config("DATABASE_HOSTNAME")
+DATABASE_CREDENTIALS = config("DATABASE_CREDENTIALS", cast=Secret)
+#handle special characters in credentials
+_DATABASE_CREDENTIAL_USER, _DATABASE_CREDENTIAL_PASSWORD = str(DATABASE_CREDENTIALS).split(":")
+_QUOTED_DATABASE_PASSWORD = parse.quote(str(_DATABASE_CREDENTIAL_PASSWORD))
+DATABASE_NAME = config("DATABASE_NAME", default="todolist")
+DATABASE_PORT = config("DATABASE_PORT", default="5432")
+DATABASE_ENGINE_MAX_OVERFLOW = config("DATABASE_ENGINE_MAX_OVERFLOW", cast=int, default=10)
+# Deal with DB disconnects
+DATABASE_ENGINE_POOL_PING = config("DATABASE_ENGINE_POOL_PING", default=False)
+DATABASE_ENGINE_POOL_RECYCLE = config("DATABASE_ENGINE_POOL_RECYCLE", cast=int, default=1800)
+DATABASE_ENGINE_POOL_SIZE = config("DATABASE_ENGINE_POOL_SIZE", cast=int, default=5)
+DATABASE_ENGINE_POOL_TIMEOUT = config("DATABASE_ENGINE_POOL_TIMEOUT", cast=int, default=10)
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{_DATABASE_CREDENTIAL_USER}:{_QUOTED_DATABASE_PASSWORD}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}"
