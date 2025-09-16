313e9d7d26eb3a8565a1a016fa74fe9969166116
Abdulroqeeb Bakre <walebakre10@gmail.com>
2025-09-16 23:54:51 +0100

    feat: tasks wip

--- src/todolist/tasks/models.py
+++ src/todolist/tasks/models.py
@@ -1,27 +1,49 @@
 
-from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, select, func
 from sqlalchemy.orm import relationship
 
 from todolist.database.core import Base
 from todolist.models import TimeStampMixin, NameStr
 from todolist.auth.models import TodolistUser, ToDoListBase
 
+from todolist.database.core import DbSession
 
-class TodolistuserTask(Base, TimeStampMixin):
+from .service import (
+    get_task_count
+)
+
+class Todolist(Base, TimeStampMixin):
     """SQLAlchemy model for the relationship between users and tasks"""
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("todolist_user.id"), nullable=False)
+    title = Column(String, nullable=False)
+
+    user = relationship("TodolistUser", back_populates="todolist")
+    task = relationship("TodolistTasks", back_populates="todolist", cascade="all, delete-orphan")
+
+
+
+class TodolistTask(Base, TimeStampMixin):
+    """SQLAlchemy model that links tasks to a list"""
+
+    id = Column(Integer, primary_key=True)
+    list_id = Column(Integer, ForeignKey("todolist.id"), nullable=False)
     task_title = Column(String, nullable=False)
-    task_description = Column(Text, nullable=True)
+    task_details = Column(Text, nullable=True)
     is_completed = Column(Boolean, default=False)
+    is_starred = Column(Boolean, default=False)
 
-    user = relationship(TodolistUser, back_populates="task")
+    todolist = relationship("Todolist", back_populates="task")
 
+class TodolistCreate(ToDoListBase):
+    """Pydaantic model for user todolist"""
 
-class UserTasks(ToDoListBase):
-    """Pydaantic model for user tasks"""
+    id: int
+    title: str
 
-    task_title: NameStr
-    task_description: NameStr
-    is_completed: bool
+class TodotaskCreate(ToDoListBase):
+    """Pydantic model for tasks"""
+    id: int
+    task_title: str
+    task_details: str | None = None
--- src/todolist/tasks/service.py
+++ src/todolist/tasks/service.py
@@ -1,10 +1,35 @@
 
+from sqlalchemy import select, func
 
-from .models import TodolistuserTask
+from todolist.database.core import DbSession
 
-def get(*, db_session, task_id: int) -> TodolistuserTask:
-    """Returns a task based on the given task id"""
-    return db_session.query(TodolistuserTask).filter(TodolistuserTask.id == task_id).one_or_none()
+from .models import Todolist, TodolistTask, TodolistCreate
 
-def create(*, db_session):
-    pass
+def get(*, db_session, list_id: int) -> Todolist:
+    """Returns a Todo list"""
+    return db_session.query(Todolist).filter(Todolist.id == list_id)
+
+def get_tasks(*, db_session, list_id: int) -> TodolistTask:
+    """Returns the tasks linked to a TodoList"""
+    return db_session.query(TodolistTask).filter(TodolistTask.list_id == list_id).all()
+
+def get_completed(*, db_session) -> TodolistTask:
+    """Return completed Tasks"""
+    return db_session.query(TodolistTask).filter(TodolistTask.is_completed == True).all()
+
+def create_list(*db_session, list_in: TodolistCreate) -> Todolist:
+    """Creates a Todolist for user with id user_id"""
+    todolist = Todolist(
+        **list_in.model_dump()
+    )
+    
+    db_session.add(todolist)
+    db_session.commit()
+    db_session.refresh(todolist)
+    
+    return todolist
+
+
+# def get_task_count(db_session: DbSession, list_id: int):
+#     stmt = select(func.count()).where(TodolistTask.list_id == list_id)
+#     return db_session.scalar(stmt)
